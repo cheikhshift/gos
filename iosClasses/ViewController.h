dc72294a724b3cea5180ee700f79da078af59551
Cheikh Seck <Adrian@OrendaCapitals-Mac-mini.local>
2015-12-28 13:40:10 -0500

    Python And iOS inj.

---
+++ iosClasses/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  Go Mobile Test
+//
+//  Created by OrendaCapital on 12/13/15.
+//  Copyright © 2015 Cheikh Seck LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property BOOL override;
+@property NSString * viewurl;
+
+@end
+
